4ceb1d2c128beb8902028d03ab05112e5462fa72
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-17 15:18:46 -0700

    defined myBind

--- callbacks_exercises.js
+++ callbacks_exercises.js
@@ -48,27 +48,55 @@ class Clock {
 // const clock = new Clock();
 
 
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-function addNumbers(sum, numsLeft, completionCallback) {
-    if (numsLeft > 0) {
-        this.response = rl.question('Please enter a number: ', response => {
-            let value = parseInt(response);
-            sum += value;
-            console.log(sum);
-            numsLeft -= 1;
-            addNumbers(sum, numsLeft, completionCallback);
-        }) 
-    }
-    if (numsLeft === 0) {
-        completionCallback(sum);
-        rl.close()
-    }
+// function addNumbers(sum, numsLeft, completionCallback) {
+//     if (numsLeft > 0) {
+//         this.response = rl.question('Please enter a number: ', response => {
+//             let value = parseInt(response);
+//             sum += value;
+//             console.log(sum);
+//             numsLeft -= 1;
+//             addNumbers(sum, numsLeft, completionCallback);
+//         }) 
+//     }
+//     if (numsLeft === 0) {
+//         completionCallback(sum);
+//         rl.close()
+//     }
     
-}
+// }
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+//myBind
+Function.prototype.myBind = function (context) {
+    console.log(this)
+    return ()=> {
+        this.apply(context);
+        console.log(this);
+    }
+}
+class Lamp {
+    constructor() {
+      this.name = "a lamp";
+    }
+  }
+  
+const turnOn = function() {
+    console.log("Turning on " + this.name);
+  };
+  
+const lamp = new Lamp();
+  
+turnOn(); // should not work the way we want it to
+  
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+  
+//   boundTurnOn(); // should say "Turning on a lamp"
+//   myBoundTurnOn(); // should say "Turning on a lamp"
